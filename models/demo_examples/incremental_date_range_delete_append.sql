3f5f18c308e3bf88fbeb96bf864f97070c9575d0
Matt Winkler <matt.winkler@fishtownanalytics.com>
2023-03-23 22:31:56 +0000

    example using a DELETE in pre_hook and configuring the incremental model itself to APPEND. pre hook macro contains BETWEEN logic.

---
+++ models/demo_examples/incremental_date_range_delete_append.sql
@@ -0,0 +1,13 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key='order_date',
+        incremental_strategy='append',
+        pre_hook=delete_from_incremental_table_between(this, date_column='order_date')
+    )
+}}
+
+select * 
+from {{ref('fct_orders')}}
+where order_date between '{{var('begin_date', '1992-01-01')}}' and '{{var('end_date', '1998-08-02')}}'
+order by order_date
