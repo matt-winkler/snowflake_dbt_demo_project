7abc15e361a2787abe690beb7a7e1e28763ec4cd
Matt Winkler <matt.winkler@fishtownanalytics.com>
2023-04-11 21:24:24 +0000

    rename region --> sales region. This gets held up in QA

--- models/marts/consumption/core/fct_orders.sql
+++ models/marts/consumption/core/fct_orders.sql
@@ -36,7 +36,7 @@ final as (
         orders.order_date,
         orders.customer_key,
         -- uncomment here and the join below to demonstrate pulling the region into the fct_orders model
-        dim_customers.region as region,
+        dim_customers.region as sales_region,
         orders.status_code,
         orders.priority_code,
         orders.clerk_name,
