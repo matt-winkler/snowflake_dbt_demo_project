c8212d6731bc2bf2a0f9166b598c98cb455a2b56
Matt Winkler <matt.winkler@fishtownanalytics.com>
2023-04-21 22:26:23 +0000

    test renaming triggers failure

--- models/marts/consumption/core/fct_order_items.sql
+++ models/marts/consumption/core/fct_order_items.sql
@@ -39,7 +39,7 @@ final as (
         part_supplier.nation_key,
         1 as order_item_count,
         order_item.quantity,
-        order_item.gross_item_sales_amount,
+        order_item.gross_item_sales_amount as gross_item_sales,
         order_item.discounted_item_sales_amount,
         order_item.item_discount_amount,
         order_item.item_tax_amount,
