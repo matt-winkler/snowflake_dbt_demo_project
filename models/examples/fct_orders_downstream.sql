269e5d78d86f82a9e01f7c2c6825ed194c15af7f
matt.winkler@fishtownanalytics.com <matt.winkler@fishtownanalytics.com>
2023-06-07 18:43:42 +0000

    updated comments after demo

--- models/examples/fct_orders_downstream.sql
+++ models/examples/fct_orders_downstream.sql
@@ -6,23 +6,18 @@
     - pre_hook leveraged to truncate the target table (if exists) or run a dummy select if it doesn't and needs to be recreated
     - dynamic_select_columns macro identifies the columns from the source table in order based on the information_schema
 #}
-
 {{
     config(
-        materialized='incremental',
-        on_schema_change='sync_all_columns',
-        incremental_strategy='delete+insert',
-        pre_hook="{% if is_incremental() %} truncate table {{this}} {% else %} select 1 as id {% endif %};"
+        materialized="incremental",
+        on_schema_change="sync_all_columns",
+        pre_hook="{% if is_incremental() %} truncate table {{this}} {% else %} select 1 as id {% endif %};",
     )
 }}
 
-{% set columns = 
-    dynamic_select_columns(
-        node=ref('fct_orders')
-    )
-%}
+{% set columns = dynamic_select_columns(node=ref("fct_orders")) %}
 
-select {% for col in columns %}
-         {{col}}{%- if not loop.last -%} , {%- endif -%}
-       {% endfor %}
-from {{ref('fct_orders')}} -- this could be a source() too - doesn't matter for illustration
+select
+    {% for col in columns %}
+        {{ col }}{%- if not loop.last -%}, {%- endif -%}
+    {% endfor %}
+from {{ ref("fct_orders") }}  -- this could be a source() too - doesn't matter for illustration
