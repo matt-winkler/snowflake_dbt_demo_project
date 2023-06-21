5044cb98ab2a897f73d52a59f504b44abcc390a1
Matt Winkler <matt.winkler@fishtownanalytics.com>
2023-06-21 21:29:27 +0000

    + sqlfluff and dim_customers to fct_orders

--- models/marts/consumption/core/fct_orders.sql
+++ models/marts/consumption/core/fct_orders.sql
@@ -49,8 +49,8 @@ final as (
         orders.clerk_name,
         orders.ship_priority,
         1 as order_count,
-        order_item_summary.return_count,
-        order_item_summary.gross_item_sales_amount,
+        order_item_summary.return_count
+        ,order_item_summary.gross_item_sales_amount,
         order_item_summary.item_discount_amount,
         order_item_summary.item_tax_amount,
         order_item_summary.net_item_sales_amount
