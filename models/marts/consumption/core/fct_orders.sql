e37257d54f731c873c89db4b8ea05fb76f81fcd1
Matt Winkler <matt.winkler@fishtownanalytics.com>
2023-06-22 19:15:51 +0000

    rename region --> customer_region

--- models/marts/consumption/core/fct_orders.sql
+++ models/marts/consumption/core/fct_orders.sql
@@ -43,7 +43,7 @@ final as (
         orders.order_date,
         orders.customer_key,
         -- uncomment here and the join below to demonstrate pulling the region into the fct_orders model
-        customers.region,
+        customers.region as customer_region,
         orders.status_code,
         orders.priority_code,
         orders.clerk_name,
