eb86f353ee8cf64f761b2d0045abfc98a55520ed
Matt Winkler <matt.winkler@fishtownanalytics.com>
2023-07-24 16:08:08 +0000

    move raw/ under models/ and cleanup unused stuff.

--- macros/config/incrementals/generate_incremental_predicates.sql
+++ macros/config/incrementals/generate_incremental_predicates.sql
@@ -1,15 +1,15 @@
 {% macro generate_incremental_predicates(date_column) %}
 
-{% set begin_date_predicate %}
+  {% set begin_date_predicate %}
      {{date_column}} >= '{{var('begin_date', '1992-01-01')}}'
-{% endset %}
+  {% endset %}
 
-{% set end_date_predicate %}
+  {% set end_date_predicate %}
      {{date_column}} <= '{{var('end_date', '1998-08-02')}}'; 
-{% endset %}
+  {% endset %}
 
-{% set predicate_array = [begin_date_predicate, end_date_predicate] %}
+  {% set predicate_array = [begin_date_predicate, end_date_predicate] %}
 
-{{ return(predicate_array) }}
+  {{ return(predicate_array) }}
 
 {% endmacro %}
