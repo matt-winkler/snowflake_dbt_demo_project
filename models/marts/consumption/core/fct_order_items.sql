4c488a36cda363c8b97b44143e38403ac901ba85
Matt Winkler <matt.winkler@fishtownanalytics.com>
2023-09-27 21:21:19 +0000

    try changing groups again

--- models/marts/consumption/core/fct_order_items.sql
+++ models/marts/consumption/core/fct_order_items.sql
@@ -38,7 +38,7 @@ final as (
         part_supplier.cost as supplier_cost,
         {# ps.retail_price, #}
         --part_supplier.retail_price,
-        --order_item.base_price,
+        order_item.base_price,
         order_item.discount_percentage,
         order_item.discounted_price,
         order_item.tax_rate,
