d38fa7a5f97a0ded4bdadc293e5e22f6a4274a7d
Matt Winkler <matt.winkler@fishtownanalytics.com>
2023-10-30 18:26:36 +0000

    remove some random columns

--- models/marts/consumption/core/fct_order_items.sql
+++ models/marts/consumption/core/fct_order_items.sql
@@ -37,8 +37,8 @@ final as (
         order_item.ship_mode,
         part_supplier.cost as supplier_cost,
         {# ps.retail_price, #}
-        part_supplier.retail_price,
-        order_item.base_price,
+        --part_supplier.retail_price,
+        --order_item.base_price,
         order_item.discount_percentage,
         order_item.discounted_price,
         order_item.tax_rate,
