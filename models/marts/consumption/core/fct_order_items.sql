89194827069435ec6529ed8a9eee813b5ff90305
Matt Winkler <matt.winkler@fishtownanalytics.com>
2023-11-17 20:49:58 +0000

    added retail_price to fct_order_items

--- models/marts/consumption/core/fct_order_items.sql
+++ models/marts/consumption/core/fct_order_items.sql
@@ -13,9 +13,16 @@ with order_item as (
     select * from {{ ref('order_items') }}
     
 ),
+
 part_supplier as (
-    
-    select * from {{ ref('part_suppliers') }}
+
+    select * from {{ ref('stg_tpch_part_suppliers')}}
+
+),
+
+dim_parts as (
+
+    select * from {{ ref('dim_parts') }}
 
 ),
 
@@ -36,23 +43,24 @@ final as (
         order_item.receipt_date,
         order_item.ship_mode,
         part_supplier.cost as supplier_cost,
-        {# ps.retail_price, #}
-        --part_supplier.retail_price,
         order_item.base_price,
         order_item.discount_percentage,
         order_item.discounted_price,
         order_item.tax_rate,
-        part_supplier.nation_key,
         1 as order_item_count,
         order_item.quantity,
         order_item.gross_item_sales_amount,
         order_item.discounted_item_sales_amount,
         order_item.item_discount_amount,
         order_item.item_tax_amount,
-        order_item.net_item_sales_amount
+        order_item.net_item_sales_amount,
+        
+        dim_parts.retail_price,
 
     from
         order_item
+        inner join dim_parts 
+            on order_item.part_key = dim_parts.part_key
         inner join part_supplier
             on order_item.part_key = part_supplier.part_key and
                 order_item.supplier_key = part_supplier.supplier_key
--- models/raw/snapshots/source_data.sql
+++
@@ -1,9 +0,0 @@
-
--- original data
-select 1 as id, 'foo' as col1, '2022-01-01 00:00:00'::timestamp as updated_at union all
-select 2 as id, 'bar' as col1, '2022-01-01 00:00:00'::timestamp as updated_at
-union all
-select 3 as id, 'baz' as col1, '2022-01-01 00:00:00'::timestamp as updated_at
-
--- uncomment to update the record with id = 1 for snapshot illustration
---union all select 1 as id, 'qux' as col1, '2022-01-01 01:00:00'::timestamp as updated_at 
