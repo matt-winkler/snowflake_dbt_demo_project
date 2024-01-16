5240a01b83c73476bcbdb110692e0383a48a97f4
Matt Winkler <matt.winkler@fishtownanalytics.com>
2024-01-16 18:47:54 +0000

    added new snapshot and dynamic date on fct_order_items

--- snapshots/source_data__snapshot.sql
+++ snapshots/source_data__snapshot.sql
@@ -3,13 +3,16 @@
     {{
         config(
             unique_key='id',
-            strategy='timestamp',
+            strategy='check',
+            check_cols=['region'],
             updated_at='updated_at',
             target_schema=target.schema,
             target_database=target.database,
         )
     }}
 
-    select * from {{ ref('source_data') }}
+    select 1 as id, 'west' as region, '2024-01-01 00:00:00' as updated_at union all
+    select 2 as id, 'central' as region, '2024-01-01 00:00:00' as updated_at 
+    --union all select 1 as id, 'midwest' as region, '2024-01-02 00:00:00' as updated_at
 
  {% endsnapshot %}
