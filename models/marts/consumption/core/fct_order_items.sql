dc571ff54e23cfc18558e3e9503a6f816e516073
Matt Winkler <matt.winkler@fishtownanalytics.com>
2024-01-24 15:48:55 +0000

    introduce an error from a missing comma

--- models/marts/consumption/core/fct_order_items.sql
+++ models/marts/consumption/core/fct_order_items.sql
@@ -49,7 +49,7 @@ final as (
         order_item.gross_item_sales_amount,
         order_item.discounted_item_sales_amount,
         order_item.item_discount_amount,
-        order_item.item_tax_amount,
+        order_item.item_tax_amount
         order_item.net_item_sales_amount
 
     from
