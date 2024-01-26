859ea9d69e7d857a2f8cd991f5c6811b487c737d
Matt Winkler <matt.winkler@fishtownanalytics.com>
2024-01-26 19:01:51 +0000

    move the hook back to on run end

--- macros/grant_usage_rights.sql
+++ macros/grant_usage_rights.sql
@@ -1,5 +1,6 @@
-{% macro grant_all_on_schemas(schemas, role) %}
-  {% for schema in schemas %}
+{% macro grant_usage_rights(database_schemas, role) %}
+  {% for (database, schema) in database_schemas %}
+    grant usage on database {{ database }} to role {{ role }};
     grant usage on schema {{ schema }} to role {{ role }};
     grant select on all tables in schema {{ schema }} to role {{ role }};
     grant select on all views in schema {{ schema }} to role {{ role }};
