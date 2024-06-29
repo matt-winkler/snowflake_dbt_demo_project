e4e649c6e1d6d292292754b033038a748a0eaae2
Matt Winkler <matt.winkler@fishtownanalytics.com>
2024-06-29 01:55:17 +0000

    allow for specifying at the node level in addition to column level

--- macros/constraints/constraints.sql
+++ macros/constraints/constraints.sql
@@ -1,28 +1,57 @@
 {%- macro create_constraints(results, graph) -%}
   {% if execute %}
+    
     {%- set model_nodes = graph['nodes'].values() | selectattr("resource_type", "equalto", "model") -%}
     {%- for res in results -%}
-    {%- if res.node.config.materialized in ['table', 'incremental', 'snapshot', 'seed'] -%}
-      {%- set table_identifier = res.node.database ~ '.' ~ res.node.schema ~ '.' ~ res.node.alias -%}
-      {%- do log('on node: ' ~ table_identifier, info=true) -%}
+      {%- if res.node.config.materialized in ['table', 'incremental', 'snapshot', 'seed'] -%}
+        {%- set table_identifier = res.node.database ~ '.' ~ res.node.schema ~ '.' ~ res.node.alias -%}
+        {%- do log('on node: ' ~ table_identifier, info=true) -%}
+        {%- set constraints = [] -%}
+        
+        {# extract node level constraints #}
+        {%- set node_constraints = res.node.config.meta.constraints -%}
+        {%- if node_constraints -%}
+          {%- for node_const in node_constraints -%}
+            {%- if node_const['type'] == 'foreign_key' -%}
+              {%- do log('setting foreign key constraints at the node level is not supported. use column level specification instead', info=true) -%}
+            {%- else -%}
+              {%- do constraints.append({
+                'type': node_const['type'], 
+                'columns': node_const['columns']
+              }) -%}
+            {%- endif -%}
+          {%- endfor -%}
+        {%- endif -%}
+
+        {# extract the column level constraints #}
         {%- set columns = res.node.columns -%}
-          {%- for col in columns -%}
-            {%- set constraints = columns[col].meta.constraints -%}
-            {%- for const in constraints -%}
-              
-              {%- do log('found constraint: ' ~ const ~ 'on column: ' ~ col, info=true) -%}
-              
-              {%- do create_constraint_from_config(
-                model_nodes, 
-                constraint=const, 
-                table_identifier=table_identifier, 
-                column_names=[col])
-              -%}
-            
+        {%- for col in columns -%}
+          {%- set col_constraints = columns[col].meta.constraints -%}
+          {%- if col_constraints -%}
+            {%- for col_const in col_constraints -%}
+              {%- do constraints.append({
+                'type': col_const,
+                'columns': [col]
+              }) -%}
             {%- endfor -%}
-          {%- endfor %}
-    {%- endif -%}
+          {%- endif -%}
+        {%- endfor %}
+        
+        {# apply extracted constraints #}
+        {%- for const in constraints -%}
+          {%- do log('applying constraint: ' ~ const ~ 'on column: ' ~ col, info=true) -%}
+          {%- do create_constraint_from_config(
+            model_nodes, 
+            constraint=const['type'], 
+            table_identifier=table_identifier, 
+            column_names=const['columns'])
+          -%}
+      
+        {%- endfor -%}
+
+      {%- endif -%}
     {%- endfor -%}
+
   {%- endif -%}
 {%- endmacro -%}
 
