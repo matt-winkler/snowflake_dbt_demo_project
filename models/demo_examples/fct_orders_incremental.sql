9836e9d42dade87de158adbcb4f49b8f97cccd9a
Matt Winkler <matt.winkler@fishtownanalytics.com>
2024-10-02 15:30:24 +0000

    microbatch fct_orders_incremental and remove warehouse resize run hooks

--- models/demo_examples/fct_orders_incremental.sql
+++ models/demo_examples/fct_orders_incremental.sql
@@ -2,17 +2,13 @@
 {{
     config(
         materialized="incremental",
-        incremental_strategy='merge',
-        unique_key='order_key',
-        on_schema_change="fail",
-        snowflake_warehouse=get_incremental_model_warehouse()
+        incremental_strategy='microbatch',
+        event_time='order_date',
+        batch_size='year',
+        begin='1992-01-01',
+        snowflake_warehouse='TRANSFORMING'
     )
 }}
 
-{% set columns = dynamic_select_columns(node=ref("fct_orders", v='0')) %}
-
-select
-    {% for col in columns %}
-        {{ col }}{%- if not loop.last -%}, {%- endif -%}
-    {% endfor %}
-from {{ ref("fct_orders", v='0') }}
+select * 
+from {{ref('fct_orders')}}
