6b2b8a9ed940cfb2b9f4b055d5b467c75643ccf4
matt-winkler <75497565+matt-winkler@users.noreply.github.com>
2025-05-01 21:26:09 -0600

    add shipment_counts.sql from canvas

--- models/marts/shipment_counts.sql
+++ models/marts/shipment_counts.sql
@@ -11,17 +11,18 @@ WITH fct_order_items AS (
     RETURN_FLAG <> 'returned' AND RETURN_FLAG <> 'awaiting return'
 ), formula_1 AS (
   SELECT
-    *
+    *,
+    TO_CHAR(order_date, 'YYYY-MM') AS order_year_month
   FROM filter_1
 ), aggregate_1 AS (
   SELECT
-    SHIP_DATE,
     SHIP_MODE,
+    order_year_month,
     COUNT(ORDER_KEY) AS num_shipments
   FROM formula_1
   GROUP BY
-    SHIP_DATE,
-    SHIP_MODE
+    SHIP_MODE,
+    order_year_month
 ), shipment_counts_sql AS (
   SELECT
     *
