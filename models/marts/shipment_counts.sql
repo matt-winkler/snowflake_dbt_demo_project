3d6fa9577219e15597fa6ff862f6b510f2967b81
matt-winkler <75497565+matt-winkler@users.noreply.github.com>
2025-06-17 11:14:25 -0600

    Auto commit for Studio handoff

--- models/marts/shipment_counts.sql
+++ models/marts/shipment_counts.sql
@@ -3,30 +3,30 @@ WITH fct_order_items AS (
   SELECT
     *
   FROM {{ ref('snowflake_hub', 'fct_order_items') }}
-), filter_1 AS (
+), exclude_returns AS (
   SELECT
     *
   FROM fct_order_items
   WHERE
     RETURN_FLAG <> 'returned' AND RETURN_FLAG <> 'awaiting return'
-), formula_1 AS (
+), format_dates AS (
   SELECT
     *,
     TO_CHAR(order_date, 'YYYY-MM') AS order_year_month
-  FROM filter_1
-), aggregate_1 AS (
+  FROM exclude_returns
+), group_by_ship_mode_and_year_month AS (
   SELECT
     SHIP_MODE,
     order_year_month,
     COUNT(ORDER_KEY) AS num_shipments
-  FROM formula_1
+  FROM format_dates
   GROUP BY
     SHIP_MODE,
     order_year_month
 ), shipment_counts_sql AS (
   SELECT
     *
-  FROM aggregate_1
+  FROM group_by_ship_mode_and_year_month
 )
 SELECT
   *
