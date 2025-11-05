8c4aecb1ef488cd5636fa642387ae92925ee5585
matt-winkler <75497565+matt-winkler@users.noreply.github.com>
2025-11-05 11:13:25 -0700

    Auto commit for Studio handoff

--- models/raw/tpch/stg_tpch_orders.sql
+++ models/raw/tpch/stg_tpch_orders.sql
@@ -1,26 +1,30 @@
-
-with source as (
-
-    select * from {{ source('tpch', 'orders') }}
-
-),
-
-rename as (
-
-    select
-    
-        o_orderkey as order_key,
-        o_custkey as customer_key,
-        o_orderstatus as status_code,
-        o_totalprice as total_price,
-        o_orderdate as order_date,
-        o_orderpriority as priority_code,
-        o_clerk as clerk_name,
-        o_shippriority as ship_priority,
-        o_comment as comment
-
-    from source
-
+WITH orders AS (
+  SELECT
+    *
+  FROM {{ source('tpch', 'orders') }}
+), rename_1 AS (
+  SELECT
+    O_ORDERKEY AS ORDER_KEY,
+    O_CUSTKEY AS CUSTOMER_KEY,
+    O_ORDERSTATUS AS STATUS_CODE,
+    O_TOTALPRICE AS TOTAL_PRICE,
+    O_ORDERDATE AS ORDER_DATE,
+    O_ORDERPRIORITY AS PRIORITY_CODE,
+    O_CLERK AS CLERK_NAME,
+    O_SHIPPRIORITY AS SHIP_PRIORITY,
+    O_COMMENT AS COMMENT
+  FROM orders
+), filter_1 AS (
+  SELECT
+    *
+  FROM rename_1
+  WHERE
+    TOTAL_PRICE > 0
+), stg_tpch_orders AS (
+  SELECT
+    *
+  FROM filter_1
 )
-
-select * from rename
+SELECT
+  *
+FROM stg_tpch_orders
